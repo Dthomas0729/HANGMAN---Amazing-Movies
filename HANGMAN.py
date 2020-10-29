77c5a6a259f7af5ce33efe42a783228a4b4dbfc2
Dthomas0729 <slicctech@gmail.com>
2020-10-28 21:48:31 -0400

    10/28/20

--- HANGMAN.py
+++ HANGMAN.py
@@ -9,17 +9,16 @@ guessed_letters = []
 g_letter = ''
 final_board = ''
 
-
+# GENERATES A RANDOM MOVIE FROM MOVIES.TXT
 def word_gen():
     global word
-    # GENERATES A RANDOM MOVIE FROM MOVIES.TXT
 
     movies = open('movies.txt').readlines()
     word = random.choice(movies)
 
 
-def guess_letter():
     # THIS IS HOW THE USER WILL GUESS LETTERS
+def guess_letter():
 
     global guess_count
     global g_letter
@@ -28,23 +27,33 @@ def guess_letter():
     g_letter = input('Choose a letter a-z: ').upper()
 
 
+# THIS IS HOW HANGMAN CHECKS IF A LETTER IS VALID
 def guess_check():
     global g_letter
     global guess_count
 
     while True:
-        if g_letter == word:
-            print('You have successfully guessed the movie!')
 
-        elif g_letter in guessed_letters:
+        # CHECKS IF LETTER IS ALREADY USED
+        if g_letter in guessed_letters:
             g_letter = input('This letter has been used already. Guess Again: ').upper()
+
+        # CHECKS FOR MULTIPLE LETTERS OR MOVIE
         elif len(g_letter) > 1:
-            g_letter = input('You may only guess 1 letter at a time. Guess Again: ').upper()
+            if g_letter.upper() == word[0:-1]:
+                print('You have successfully guessed the movie!')
+                board_gen()
+                break
+            else:
+                print(len(word))
+                g_letter = input('You may only guess 1 letter at a time. Guess Again: ').upper()
+
         elif g_letter in word:
             guessed_letters.append(g_letter)
             print(' \n  LETTER FOUND! \n')
             board_gen()
             break
+
         elif g_letter not in word:
             guessed_letters.append(g_letter)
             print(' \n  LETTER NOT FOUND \n')
@@ -53,7 +62,7 @@ def guess_check():
             break
     count_check()
 
-
+# CREATES AND UPDATES WORD BOARD THAT PLAYER SEES
 def board_gen():
     global final_board
     new_board = ''
@@ -79,21 +88,15 @@ def check_win():
         playing = False
         replay()
 
-
+# COUNTS HOW MANY ATTEMPTS ARE LEFT
 def count_check():
     global guess_count
     if guess_count == 0:
         print('\nYOU HAVE ZERO GUESSES LEFT!')
-        code = input('\nIf you would like to continue, \n'
-                     'Give your boyfriend a BIG SMOOCH!\n\n'
-                     '(He will give you the code) ENTER HERE: ')
-        if code == 'baby':
-            guess_count = 6
-        else:
-            playing = False
-            print('GAME OVER***GOOD TRY\n')
-            print(word + 'was the correct movie\n_________________________')
-            replay()
+        playing = False
+        print('GAME OVER***GOOD TRY\n')
+        print(f'The movie is... {word}')
+        replay()
 
 
 def replay():
